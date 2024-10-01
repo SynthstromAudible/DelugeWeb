630c280912f4f72fdfa12309ecb99beb17485f6a
Jamie Fenton <jamie@fentonia.com>
2024-10-01 16:35:06 -0500

    Improved file routines & testers.

--- cascade/src/DirView.vue
+++ cascade/src/DirView.vue
@@ -3,37 +3,12 @@
   import {sendJsonRequest} from "./JsonReplyHandler.js";
   import DirEntry from "./DirEntry.vue";
   import {openEditOn, editPath} from "./common.js";
+  import {getDirInfo} from "./FileRoutines.js";
 
   let dirList = ref([]);
-  const blkSize = 20;
 	let dirpath = ref("/");
 	let showHidden = ref(true);
-	
-	function getDirInfo() {
-		let working = [];
-		dirList.value = [];
-		let path = dirpath.value;
-	
-    let dirCB = function(verb, object, data, payoff) {
-			let chunk = object[verb];
-			working = working.concat(chunk);
-		  if (chunk.length > 0) {
-	
-				  let params = {offset: working.length, lines: blkSize, path: path};
-				  sendJsonRequest("dir", params, dirCB);
-		  } else {
-		  dirList.value = working;
-		}
-	}
 
-		 let params = {offset: 0, lines: blkSize, path: path};
-		 params.offset = 0;
-		 params.lines = blkSize;
-		 params.path = dirpath.value;
-
-		 sendJsonRequest("dir", params, dirCB);
-	}
-	
 let cdUpItem = {name: "..", attr: 0x10};
 
  function changeDir(child)
@@ -52,7 +27,9 @@ let cdUpItem = {name: "..", attr: 0x10};
   }
   console.log(newP);
   dirpath.value = newP;
-  getDirInfo();
+  getDirInfo(newP, (err, plist)=>{
+  		dirList.value = plist;
+  });
  }
  
 function openPath(dEntry) {
@@ -61,10 +38,16 @@ function openPath(dEntry) {
 	openEditOn(fullPath);
 }
 
+function dirGetter() {
+   getDirInfo(dirpath.value, (err, plist)=>{
+  		dirList.value = plist;
+  	});
+}
+
 </script>
 
 <template>
-<button type="button" id="getDirButton" @click="getDirInfo">Get Dir List</button><p/>
+<button type="button" id="getDirButton" @click="dirGetter">Get Dir List</button><p/>
 <input v-model="dirpath">&nbsp;
 <input type="checkbox" id="checkbox" label="Show hidden files" v-model="showHidden">
 <label for="checkbox">Show hidden</label><p/><br/>
